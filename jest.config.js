c643b03ba0edba80811b2199a9f27ad5b36273a9
Derek Nola <derek.nola@suse.com>
2024-04-10 11:18:35 -0700

    Update to node20, bump dependencies
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
   verbose: true,
-  collectCoverage: true
+  collectCoverage: true,
+  moduleNameMapper: {
+    '@octokit/auth-action': '<rootDir>/mocks/auth-action.js',
+  }
 }
